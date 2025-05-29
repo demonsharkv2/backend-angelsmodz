3cf1ef7560d61ab5c589cdda027a4a7601fdd4d2
demonsharkv2 <tek.angel.v2@gmail.com>
2025-05-29 10:20:27 -0300

    Update Usuario.js

--- models/Usuario.js
+++ models/Usuario.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const UsuarioSchema = new mongoose.Schema({
   numero: { type: String, required: true, unique: true },
   nome: { type: String, required: true },
-  senha: { type: String, required: true }
+  senha: { type: String, required: true },
 });
 
 module.exports = mongoose.model('Usuario', UsuarioSchema);
